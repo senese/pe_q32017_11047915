4f41b3773ad717573dec4d807e5ddd8a27cb623c
Diego Senese <senese.diego@aluno.ufabc.edu.br>
2017-09-25 18:45:47 +0000

    exercicio 3

---
+++ lista01/03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, soma;
+    scanf("%d%d", &x, &y);
+    soma = x * y;
+    
+    printf("A multiplicação dos dois números inseridos é: %d\n", soma);
+    return 0;
+}
