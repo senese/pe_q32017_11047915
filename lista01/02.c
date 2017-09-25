e3314f4702baae5018f9b23d01ac4154bcacd6b5
Diego Senese <senese.diego@aluno.ufabc.edu.br>
2017-09-25 18:43:19 +0000

    exercicio 2

---
+++ lista01/02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, soma;
+    scanf("%d%d", &x, &y);
+    soma = x + y;
+    
+    printf("A soma dos dois números inseridos é: %d\n", soma);
+    return 0;
+}
