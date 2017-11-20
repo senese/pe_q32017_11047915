7f706e0ebca525e9a640479d0d1c1a50bd4ff95a
senese <diegosenese@hotmail.com>
2017-11-20 15:29:21 -0200

    exercicio 3

--- lista04/03.c
+++ lista04/03.c
@@ -4,19 +4,45 @@
 typedef struct Aluno {
     char nome[100];
     int RA;
-    int p1;
-    int p2;
-    int p3;
+    float p1;
+    float p2;
+    float p3;
+    float media;
 } Aluno;
+
+void media(Aluno *alunos)
+{
+	int i;
+	float soma;
+	for (i=0; i<50; i++) {
+		soma = alunos[i].p1 + alunos[i].p2 + alunos[i].p3;
+		alunos[i].media = soma/3;
+	}
+}
 
 int main(int argc, char** argv)
 {
+	int i;
     Aluno alunos[50];
     FILE *lista;
-    
+
     lista = fopen(argv[1], "r");
-    
+
+	for (i=0; i<50; i++) {
+    	fscanf(lista, "%s %d %f %f %f", &alunos[i].nome, &alunos[i].RA, &alunos[i].p1, &alunos[i].p2, &alunos[i].p3);
+	}
+
     fclose(lista);
-    
+
+	media(alunos);
+
+	lista = fopen("media_alunos.txt","w");
+
+	for (i=0; i<50; i++) {
+		fprintf(lista, "%s 00%d %.1f %.1f %.1f %.2f\n", alunos[i].nome, alunos[i].RA, alunos[i].p1, alunos[i].p2, alunos[i].p3, alunos[i].media);
+	}
+
+	fclose(lista);
+
     return 0;
 }
