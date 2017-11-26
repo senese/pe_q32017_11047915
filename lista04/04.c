550cc0d092851279c2bbf6dfbbc7c297c760fda5
senese <diegosenese@hotmail.com>
2017-11-26 18:18:38 -0200

    exercicio 4

--- lista04/04.c
+++ lista04/04.c
@@ -2,42 +2,54 @@
 #include <time.h>
 #include <limits.h>
 
-int f1(int x)
+#define BIGNUM ULONG_MAX
+
+typedef unsigned long int ulint;
+
+ulint f1(ulint x)
 {
-	int i;
-	while(x%2 == 0)
-}
-
-int f2(int x)
-{
-	return x >> 3;
-}
-
-int main(void)
-{
-	clock_t tempo_init, tempo_fim;
-	double tempo_gasto;
-	int a, n;
-	scanf("%d", &n);
-
-	tempo_init = clock();
-	a = f1(n);
-	if (a == 1) printf("sim\n");
-	else		printf("nao\n");
-	tempo_fim = clock();
-
-	tempo_gasto = (double)(tempo_fim - tempo_init) / CLOCKS_PER_SEC;
-	printf("Tempo gasto na versao normal: %lf\n", tempo_gasto);
-
-
-	tempo_init = clock();
-	a = f2(n);
-	if (a == 1) printf("sim\n");
-	else		printf("nao\n");
-	tempo_fim = clock();
-
-	tempo_gasto = (double)(tempo_fim - tempo_init) / CLOCKS_PER_SEC;
-	printf("Tempo gasto na versao bitwise: %lf\n", tempo_gasto);
-
+	if (x > 0) {
+		while((x%2) == 0) {
+			x /= 2;
+			if (x == 1) return 1;
+		}
+	}
 	return 0;
+}
+
+
+ulint f2(ulint x)
+{
+	ulint soma = 0;
+	while (x && (soma<=1)) {
+		soma += (x&1);
+		x >>= 1;
+	}
+	return (soma==1);
+}
+
+int main(void) {
+  
+  clock_t tempo_init, tempo_fim;
+  double tempo_gasto;
+  ulint soma = 0;
+  int i;
+  
+  tempo_init = clock();
+  for (i=0; i<BIGNUM; i++) {
+    soma += f1(i);
+  }
+  tempo_fim = clock();
+  tempo_gasto = (double)(tempo_fim - tempo_init) / CLOCKS_PER_SEC;
+  printf("Tempo gasto na versao normal: %lf\n", tempo_gasto);
+  
+  tempo_init = clock();
+  for (i=0; i<BIGNUM; i++) {
+    soma += f2(i);
+  }
+  tempo_fim = clock();
+  tempo_gasto = (double)(tempo_fim - tempo_init) / CLOCKS_PER_SEC;
+  printf("Tempo gasto na versao bitwise: %lf\n", tempo_gasto);
+  
+  return 0;
 }
